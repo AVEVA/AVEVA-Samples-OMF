58e3609e3a593f3a49d7289c61144158745223d0
Derek Endres <dendres@osisoft.com>
2019-09-06 13:08:50 -0400

    tests

--- basic_samples/OMF_API/Python3/program.py
+++ basic_samples/OMF_API/Python3/program.py
@@ -241,7 +241,12 @@ def checkValueGone(url):
         response.close()
         print('Value found.  This is unexpected.  "{0}"'.format(response.status_code))
         print()
-        raise Exception("Check message was failed. {status}:{reason}".format( status=response.status_code, reason=response.text))
+        opId = response.headers["Operation-Id"]
+        status=response.status_code
+        reason=response.text
+        url = response.url
+        error = f"  {status}:{reason}.  URL {url}  OperationId {opId}"
+        raise Exception(f"Check message was failed. {error}")
     return response.text
    
 
@@ -757,7 +762,7 @@ def checkDeletes():
     time.sleep(2)
 
     if(sendingToOCS):
-        time.sleep(5)
+        #time.sleep(5)
         checkValueGone(checkBase + '/Streams' + '/Container1')
     else:
         json1 = checkValue(checkBase + "/dataservers?name=" + dataServerName)
